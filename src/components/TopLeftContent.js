7a79f78fdfcef8eac21d8e562fab0b86368d384e
chethana_p_n <chethana.pn@yethi.in>
2024-04-17 00:49:50 +0530

    Content changes

--- src/components/TopLeftContent.js
+++ src/components/TopLeftContent.js
@@ -2,22 +2,24 @@ import React from "react";
 
 const TopLeftContent = () => {
   return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          color: "white",
-        }}
-      >
-        <h2>CALIBRE ENGINEERING</h2>
-        <div style={{ overflowWrap: "break-word" }}>
-          This extension allows you to generate Lorem Ipsum text directly within
-          your editor. You can customize the number of paragraphs, sentences, or
-          words you want. You can install it from the VSCode marketplace by
-          searching for "Lorem Ipsum" and clicking on the install button.
-        </div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        color: "white",
+      }}
+    >
+      <h2>CALIBRE ENGINEERING</h2>
+      <div style={{ overflowWrap: "break-word" }}>
+        Pexels is a popular online platform that offers a vast collection of
+        high-quality, royalty-free stock photos and videos. It provides content
+        creators, designers, developers, and individuals with access to a
+        diverse range of visually appealing images and footage for use in
+        various projects, including websites, blogs, presentations, social media
+        posts, and more.
       </div>
+    </div>
   );
 };
 
